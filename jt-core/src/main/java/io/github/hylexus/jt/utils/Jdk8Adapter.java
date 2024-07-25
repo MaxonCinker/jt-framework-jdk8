ba33da8997092d4c9628992a0c6e8dfdffc5996d
dujunliang <dujunliang@zhiyuanhui.com>
2024-07-25 17:16:06 +0800

    新增jdk8适配方法

--- jt-core/src/main/java/io/github/hylexus/jt/utils/Jdk8Adapter.java
+++ jt-core/src/main/java/io/github/hylexus/jt/utils/Jdk8Adapter.java
@@ -1,5 +1,8 @@
 package io.github.hylexus.jt.utils;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * @Author dujunliang
  * @Date 2024/7/25 16:14
@@ -13,6 +16,30 @@ public class Jdk8Adapter {
             sb.append(str);
         }
         return sb.toString();
+    }
+
+    public static <T> boolean optionalIsEmpty(Optional<T> optional) {
+        return !optional.isPresent();
+    }
+
+    public static NoSuchElementException optionalOrElseThrow() {
+        return new NoSuchElementException("No value present");
+    }
+
+    public static <E> Set<E> setOf(E... e) {
+        return Arrays.stream(e).collect(Collectors.toSet());
+    }
+
+    public static <E> List<E> listOf(E... e) {
+        return Arrays.stream(e).collect(Collectors.toList());
+    }
+
+    public static Map<String, Object> mapOf(Object... o) {
+        Map<String, Object> map = new HashMap<>();
+        for (int i = 0; i < o.length; i += 2) {
+            map.put(o[i].toString(), o[i + 1]);
+        }
+        return map;
     }
 
 }
