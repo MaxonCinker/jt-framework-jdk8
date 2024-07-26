0df610c332e1e11cd3d4b3cb787bc2dbed4e0777
dujunliang <dujunliang@zhiyuanhui.com>
2024-07-26 14:23:47 +0800

    修改StringBuffer不适用错误

--- jt-core/src/main/java/io/github/hylexus/jt/utils/Jdk8Adapter.java
+++ jt-core/src/main/java/io/github/hylexus/jt/utils/Jdk8Adapter.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class Jdk8Adapter {
 
     public static String stringRepeat(String str, int count) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < count; i++) {
             sb.append(str);
         }
@@ -32,14 +32,6 @@ public class Jdk8Adapter {
 
     public static <E> List<E> listOf(E... e) {
         return Arrays.stream(e).collect(Collectors.toList());
-    }
-
-    public static Map<String, Object> mapOf(Object... o) {
-        Map<String, Object> map = new HashMap<>();
-        for (int i = 0; i < o.length; i += 2) {
-            map.put(o[i].toString(), o[i + 1]);
-        }
-        return map;
     }
 
 }
